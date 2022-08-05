1d58f2b4a357a8d6ad353847d2045172c8421d12
modusmogulus <39548121+modusmogulus@users.noreply.github.com>
2022-08-05 16:23:17 +0300

    autoload gameobject --> global vars
    usage:
    
    onready var global = get_node("/root/Gameobject")
    
    print(global.<var>)

---
+++ scripts/Gameobject.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+var cecilia = "joo"
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,19 +3,17 @@ extends Area2D
 export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var global = get_node("/root/Gameobject")
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
 func _process(delta):
+
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
