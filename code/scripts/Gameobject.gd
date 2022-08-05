a97a4875118a5008310f9fa3dd2554c5047f6225
Henry Palonen <henry.palonen@palonenlabs.com>
2022-08-05 21:30:19 +0300

    preliminary icecream

--- code/scripts/Gameobject.gd
+++ code/scripts/Gameobject.gd
@@ -4,10 +4,12 @@ extends Node2D
 
 var modifier_state = 0
 onready var timer = $Timer
-var time_left = 60
+export var time_left = 23
 onready var label = $Control/Label
 
-
+func _ready():
+	timer.wait_time = time_left
+	
 func _process(delta):
 	timer.time_left
 	if timer.time_left != null:
