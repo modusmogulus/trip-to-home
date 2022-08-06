d3e444bef538a03955494e9ae4b1fd7bd6bb893f
modusmogulus <39548121+modusmogulus@users.noreply.github.com>
2022-08-06 15:31:54 +0300

    music stuff

---
+++ code/scripts/dynamic_music.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+
+export var top_height = 500
+export var min_height = 0
+onready var base_player = $base
+onready var top_player = $top
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	pass
